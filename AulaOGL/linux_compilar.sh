70499f214275f50a0c13978da355dc47aef5183c
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2022-03-24 00:40:11 -0300

    .

--- AulaOGL/linux_compilar.sh
+++ AulaOGL/linux_compilar.sh
@@ -1,2 +1,7 @@
-g++ -pthread -o a data.h colors.h main.cpp -lglfw -lGLU -lGL -lXrandr -lX11 -lrt -ldl -lglut -lGLU -lGL
+#!/bin/sh
+g++ -pthread -o a data.h colors.h main.cpp -lglfw -lGLU -lGL -lXrandr -lX11 -lrt -ldl -lglut
+
+./a
+
+rm ./a
 
